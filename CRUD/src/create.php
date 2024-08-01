e0ad8578650e4647b49d8a90f89c032a121c0376
oshlite <140532148+oshlite@users.noreply.github.com>
2024-08-01 10:59:54 +0700

    Latihan php JWD database
    create php upload foto required kolom-kolom

--- CRUD/src/create.php
+++ CRUD/src/create.php
@@ -1,16 +1,76 @@
 <?php
   require 'connect.php';
 
+  function generateRandomString($length = 10) {
+    // Karakter yang dapat digunakan
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    
+    // Menghasilkan string acak
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    
+    return $randomString;
+  }
+
   if (isset($_POST['submit'])) {
     $npm = $_POST['npm'];
     $name = $_POST['name'];
     $JenisKelamin = $_POST['JenisKelamin'];
     $tanggallahir = $_POST['tanggallahir'];
     $alamat = $_POST['alamat'];
-    //$foto = $_POST['foto'];
-    $foto = 'test.jpg';
 
-    $sql = "INSERT INTO `mahasiswa` (npm,name,JenisKelamin,tanggallahir,alamat,foto) VALUES('$npm','$name', '$JenisKelamin', '$tanggallahir', '$alamat','$foto')";
+    $target_dir = "uploads/";
+    $target_file = $target_dir  . generateRandomString() . basename($_FILES["foto"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["foto"]["tmp_name"]);
+      if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+      } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+      }
+    }
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+      echo "Sorry, file already exists.";
+      $uploadOk = 0;
+    }
+
+    // Check file size
+    if ($_FILES["foto"]["size"] > 500000) {
+      echo "Sorry, your file is too large.";
+      $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+      $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+      echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+      if (move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file)) {
+        echo "The file ". htmlspecialchars( basename( $_FILES["foto"]["name"])). " has been uploaded.";
+      } else {
+        echo "Sorry, there was an error uploading your file.";
+      }
+    }
+
+    $sql = "INSERT INTO `mahasiswa` (npm,name,JenisKelamin,tanggallahir,alamat,foto) VALUES('$npm','$name', '$JenisKelamin', '$tanggallahir', '$alamat','$target_file')";
     $result = mysqli_query($con, $sql);
 
     if ($result) {
